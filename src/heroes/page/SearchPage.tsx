08de13713c366659edccdbb34b27f3d4e0011250
mynorgc79 <mynorguarchaj99@gmail.com>
2023-08-10 21:43:42 -0600

    Add alerts Validates

--- src/heroes/page/SearchPage.tsx
+++ src/heroes/page/SearchPage.tsx
@@ -13,13 +13,16 @@ export const SearchPage = () => {
   const {q = ''} = queryString.parse(location.search);
   const heroes: Hero[] = getHeroesByName(q as string);
 
+  const showSearch:  boolean = (q!.length ===0);
+  const showError: boolean = (q!.length > 0 && heroes.length === 0)
+
   const { formState, onInputChange } = useForm({
     searchText: q as string
   });
 
   const onSearchSubmit = (event: React.FormEvent<HTMLFormElement> ) => {
     event.preventDefault();
-    if ( formState.searchText.trim().length <= 1) return;
+ // if ( formState.searchText.trim().length <= 1) return;
 
     navigate(`?q=${formState.searchText.toLowerCase().trim()}`);
   }
@@ -54,12 +57,20 @@ export const SearchPage = () => {
           <h4>Results</h4>
           <hr />
 
-          <div className="alert alert-primary">
-            Search a hero
+         { /* {
+            (q === '')
+            ? <div className="alert alert-primary">Search a hero </div>
+            :(heroes.length === 0)
+            &&  <div className="alert alert-danger">No hero with <b>{ q }</b>
           </div>
+          }*/ }
 
-          <div className="alert alert-danger">
-            No hero with <b>{ q }</b>
+            <div className="alert alert-primary animate__animated animate__fadeIn" style={{display:showSearch ? '' : 'none'}}>
+              Search a hero 
+              </div>
+
+              <div className="alert alert-danger animate__animated animate__fadeIn" style={{display: showError? '': 'none'}}>
+                No hero with <b>{ q }</b>
           </div>
 
           {
